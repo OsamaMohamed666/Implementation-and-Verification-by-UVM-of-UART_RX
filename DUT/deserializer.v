4ca1871dd5a2517e7f5d1d4f14c0decfa9b02988
Osama Mohamed Elsheikh <osama.mohamed.elshiekh@gmail.com>
2025-04-18 13:32:11 +0200

    Update deserializer.v

--- DUT/deserializer.v
+++ DUT/deserializer.v
@@ -1,29 +1,25 @@
+module deserializer #(parameter DATA_WIDTH =8) (
 
-module deserializer # ( parameter DATA_WIDTH = 8 )
+ input             clk,
+ input             rst,
+ input             sampled_bit,
+ input             en,
+ input		[2:0]  edge_count,
+ output reg [DATA_WIDTH-1:0]  P_DATA
 
-(
- input   wire                    CLK,
- input   wire                    RST,
- input   wire                    sampled_bit,
- input   wire                    Enable, 
- input   wire   [2:0]            edge_count, 
- output  reg    [DATA_WIDTH-1:0] P_DATA
-);
+ );
 
-              
-//deserializer
-always @ (posedge CLK or negedge RST)
- begin
-  if(!RST)
-   begin
-    P_DATA <= 'b0 ;
-   end
-  else if(Enable && edge_count == 3'b111)
-   begin
-    P_DATA <= {sampled_bit,P_DATA[7:1]}	;
-   end	
- end
+ always @(posedge clk or negedge rst)
+  begin
+   if(!rst)
+     P_DATA <=0;
+   else 
+    if (en && (edge_count == 3'b111))
+        begin 
+		   P_DATA<={sampled_bit,P_DATA[7:1]};
+	    end
+  end
  
-
-endmodule
- 
+ endmodule
+  
+		   
