950329d2dcda73c6f4154bc09280086d31e9df28
Osama Mohamed Elsheikh <osama.mohamed.elshiekh@gmail.com>
2025-04-18 13:36:53 +0200

    Update strt_chk.v

--- DUT/strt_chk.v
+++ DUT/strt_chk.v
@@ -1,27 +1,32 @@
+module strt_chk(
+  
+  input            clk,
+  input            rst,
+  input            en,
+  input            sampled_bit,
+  output  reg      strt_glitch
 
-module strt_chk (
- input   wire                  CLK,
- input   wire                  RST,
- input   wire                  sampled_bit,
- input   wire                  Enable, 
- output  reg                   strt_glitch
-);
-
- 
-              
-// error check
-always @ (posedge CLK or negedge RST)
- begin
-  if(!RST)
-   begin
-    strt_glitch <= 'b0 ;
-   end
-  else if(Enable)
-   begin
-    strt_glitch <= sampled_bit ;
-   end
- end
- 
-
- 
-endmodule
+  );
+  
+  always @ (posedge clk or negedge rst)
+    begin
+	   if(!rst)
+	    strt_glitch<=0;
+	   else
+	    begin
+	      if (en)
+		   begin
+		    case(sampled_bit)
+			 1'b0 : begin
+			          strt_glitch<=0;
+					end  
+			 1'b1 : begin	
+                      strt_glitch<=1;
+					end  
+			endcase
+		   end
+        end			
+	end	
+	
+	
+endmodule 	
