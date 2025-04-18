966c48a0ba95976edb1998dd3e606527e78ae35b
Osama Mohamed Elsheikh <osama.mohamed.elshiekh@gmail.com>
2025-04-18 13:37:19 +0200

    Update uart_rx_fsm.v

--- DUT/uart_rx_fsm.v
+++ DUT/uart_rx_fsm.v
@@ -1,236 +1,157 @@
+module FSM (
 
-module uart_rx_fsm  # ( parameter DATA_WIDTH = 8 ) (
+  input             clk,
+  input             rst,
+  input             rx_in,
+  input    [2:0]    edge_cnt,
+  input    [3:0]    bit_cnt,
+  input             par_err,
+  input             par_en,
+  input             strt_glitch,
+  input             stp_err,
+  
+  output reg        enable, // edge bit count enable 
+  output reg        data_samp_en,
+  output reg        par_chk_en,
+  output reg        strt_chk_en,
+  output reg        stp_chk_en,
+  output reg        deser_en,
+  output reg        data_valid
 
- input   wire                  CLK,
- input   wire                  RST,
- input   wire                  S_DATA,
- input   wire                  parity_enable, 
- input   wire   [3:0]          bit_count,
- input   wire   [2:0]          edge_count,
- input   wire                  par_err,
- input   wire                  stp_err, 
- input   wire                  strt_glitch,
- output  reg                   strt_chk_en,  
- output  reg                   edge_bit_en, 
- output  reg                   deser_en, 
- output  reg                   par_chk_en, 
- output  reg                   stp_chk_en,
- output  reg                   dat_samp_en,
- output  reg                   data_valid
-);
-
-
-// gray state encoding
-localparam  [2:0]      IDLE     = 3'b000 ,
-                       start    = 3'b001 ,
-					   data     = 3'b011 ,
-					   parity   = 3'b010 ,
-					   stop     = 3'b110 ,
-					   err_chk  = 3'b111 ,
-					   data_vld = 3'b101 ;
-
-reg         [2:0]      current_state , next_state ;
-			
-
-//state transiton 
-always @ (posedge CLK or negedge RST)
- begin
-  if(!RST)
+  );
+  
+  //5 STATES
+  localparam        idle = 3'b000,
+                    strt_chk = 3'b001,
+                    deser = 3'b010, 
+                    par_chk = 3'b011,
+                    stp_chk = 3'b100,
+                    err_chk = 3'b101,
+                    data_vld = 3'b110;
+					
+	
+  
+  wire edge_7 , bit_8;
+  wire [2:0] deser_cond;
+  assign edge_7 = (edge_cnt == 3'b111)? 1'b1 : 1'b0;
+  assign bit_8 = (bit_cnt == 4'b1000);
+  assign deser_cond = {par_en,bit_8,edge_7};
+  
+  reg [2:0] cs,ns;  // CURRENT_STATE & NEXT_STATE
+ 
+           					 
+  always @ (posedge clk or negedge rst)
    begin
-    current_state <= IDLE ;
+    if(!rst)
+	    cs<=idle;
+	else 
+	    cs<=ns;
    end
-  else
-   begin
-    current_state <= next_state ;
-   end
- end
+   
+   // NEXT STATE LOGIC & OUTPUT LOGIC
+   always @ (*)
+    begin
+	    par_chk_en=0;
+		deser_en=0;
+		strt_chk_en=0;
+		stp_chk_en=0;
+		data_valid=0;
+		enable=1;
+		data_samp_en=1;
+		
+        case (cs)
+		  idle :     begin
+		              if(rx_in)
+					   begin
+		                 data_samp_en=0;
+					     enable=0; 
+						 ns=idle;
+					   end
+					  else
+					   begin
+					    ns=strt_chk;
+					    strt_chk_en=1;
+					   end 
+					end
+		  strt_chk : begin
+					   strt_chk_en=1;
+						if (bit_cnt=='b0 && edge_7) 
+						 begin
+						  if(strt_glitch)
+					       ns=idle;
+						  else
+                           ns=deser;
+						  end 
+						else 
+							ns = cs;
+					 end	 
+					 
+          deser :    begin
+					    deser_en=1;
+		                case(deser_cond)
+						3'b011 : begin
+						            ns=stp_chk;
+								 end 
+						3'b111 : begin
+						            ns=par_chk;
+								 end
+						default : ns=cs;
+						endcase
+		                
+					 end	
+						 
+		  par_chk :  begin 
+						par_chk_en=1;
+						if (edge_7)
+						 ns=stp_chk;
+						else
+						 ns=cs;
+					 end
+					
+		  stp_chk :  begin
+						stp_chk_en = 1;
+						 if (edge_cnt == 3'b101)
+						  ns=err_chk;
+						 else 
+						  ns = cs;
+					 end 
+		  
+		  err_chk : begin
+					enable=0;
+					 if (par_err | stp_err)
+					  ns = idle;
+					 else 
+					  ns = data_vld;
+					end 
+		  data_vld : begin
+					data_valid =1;
+					 enable =0;
+					 data_samp_en =0;
  
+					 if(!rx_in)
+					  ns = strt_chk ;
+					 else
+					  ns = idle; 						
+					end			  			
+		  default : begin 
+					 ns=idle;
+					 enable=0;
+					 deser_en=0;
+					end 
+		endcase
+	end
+	
+endmodule 
 
-// next state logic
-always @ (*)
- begin
-  case(current_state)
-  IDLE   : begin
-             if(!S_DATA)
-			  next_state = start ;
-			else
-			  next_state = IDLE ; 			
-           end
-  start  : begin
-             if(bit_count == 4'd0 && edge_count == 3'd7)
-			   begin
-			    if(!strt_glitch)                    // check that start bit not a glitch
-				 begin
-			      next_state = data ;
-                 end
-                else 
-                 begin
- 			      next_state = IDLE ;       
-                 end		
-               end
-			 else
-			   begin
-			    next_state = start ; 			
-               end			  
-           end
-  data   : begin
-             if(bit_count == 4'd8 && edge_count == 3'd7)
-			   begin
-			     if(parity_enable)
-				   begin
-			        next_state = parity ;
-				   end 
-                 else
-				   begin
-			        next_state = stop ;
-                   end					
-               end
-			 else
-			   begin
-			    next_state = data ; 			
-               end			  
-           end 
-  parity : begin
-             if(bit_count == 4'd9 && edge_count == 3'd7)
-			   begin
-			    next_state = stop ; 
-               end
-			 else
-			   begin
-			    next_state = parity ; 			
-               end			  
-           end
-  stop   : begin
-			 if (parity_enable) 
-			   begin 
-			     if(bit_count == 4'd10 && edge_count == 3'd5)
-			       begin
-			         next_state = err_chk ; 
-                   end
-			     else
-			       begin
-			         next_state = stop ; 			
-                   end	
-			   end 
-			 else 
-				begin
-				if(bit_count == 4'd9 && edge_count == 3'd5)
-			       begin
-			         next_state = err_chk ; 
-                   end
-			     else
-			       begin
-			         next_state = stop ; 			
-                   end	
-				end 
-           end	
-  err_chk   : begin
-               if(par_err | stp_err)
-			   begin
-			    next_state = IDLE ; 
-               end
-			 else
-			   begin
-			    next_state = data_vld ; 	    				
-               end			  
-             end	
-  data_vld : begin
-              if(!S_DATA)
-			   next_state = start ;
-			  else
-			   next_state = IDLE ; 						
-             end			  		   
-  default: begin
-			 next_state = IDLE ; 
-           end	
-  endcase                 	   
- end 
- 
-// output logic
-always @ (*)
- begin
-  edge_bit_en = 1'b0 ;
-  dat_samp_en = 1'b0 ;  
-  deser_en    = 1'b0 ; 
-  par_chk_en  = 1'b0 ; 
-  stp_chk_en  = 1'b0 ;
-  data_valid  = 1'b0 ;
-  strt_chk_en = 1'b0 ;
-  case(current_state)
-  IDLE   : begin
-             if(!S_DATA)
-			  begin
-               edge_bit_en = 1'b1 ; 
-               deser_en    = 1'b0 ; 
-               par_chk_en  = 1'b0 ; 
-               stp_chk_en  = 1'b0 ;	
-			   strt_chk_en = 1'b1 ;
-               dat_samp_en = 1'b1 ; 			   
-			  end 
-			else
-			  begin
-			   strt_chk_en = 1'b0 ;
-               edge_bit_en = 1'b0 ; 
-               deser_en    = 1'b0 ; 
-               par_chk_en  = 1'b0 ; 
-               stp_chk_en  = 1'b0 ;
-               dat_samp_en = 1'b0 ; 			   
-			  end  			
-           end
-  start  : begin
-			 strt_chk_en = 1'b1 ;  
-             edge_bit_en = 1'b1 ; 
-             deser_en    = 1'b0 ; 
-             par_chk_en  = 1'b0 ; 
-             stp_chk_en  = 1'b0 ;
-             dat_samp_en = 1'b1 ;			 
-           end
-  data   : begin
-             edge_bit_en = 1'b1 ; 
-             deser_en    = 1'b1 ; 
-             par_chk_en  = 1'b0 ; 
-             stp_chk_en  = 1'b0 ;
-             dat_samp_en = 1'b1 ;			 
-           end
-  parity : begin
-             edge_bit_en = 1'b1 ; 
-             deser_en    = 1'b0 ; 
-             par_chk_en  = 1'b1 ; 
-             stp_chk_en  = 1'b0 ;
-             dat_samp_en = 1'b1 ;				 
-           end
-  stop   : begin
-             edge_bit_en = 1'b1 ; 
-             deser_en    = 1'b0 ; 
-             par_chk_en  = 1'b0 ; 
-             stp_chk_en  = 1'b1 ;
-             dat_samp_en = 1'b1 ;				 
-           end	
-  err_chk: begin
-             edge_bit_en = 1'b0 ; 
-             deser_en    = 1'b0 ; 
-             par_chk_en  = 1'b0 ; 
-             stp_chk_en  = 1'b0 ;
-             dat_samp_en = 1'b1 ;				 
-           end	
-  data_vld:begin
-             edge_bit_en = 1'b0 ; 
-             deser_en    = 1'b0 ; 
-             par_chk_en  = 1'b0 ; 
-             stp_chk_en  = 1'b0 ;
-             data_valid  = 1'b1 ;
-             dat_samp_en = 1'b0 ;				 
-           end			   
-  default: begin
-             edge_bit_en = 1'b0 ; 
-             deser_en    = 1'b0 ; 
-             par_chk_en  = 1'b0 ; 
-             stp_chk_en  = 1'b0 ;	
-			 data_valid  = 1'b0 ;
-             dat_samp_en = 1'b0 ;				 
-		   end 
-  endcase                 	   
- end 
-
-endmodule
- 
+						
+				
+						 
+								    
+							
+							
+                            		  
+					
+	
+	
+	
+	
